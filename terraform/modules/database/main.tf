6f0ce968ea4132169836a813b1625cd490c1e188
Ananya Sinha <ananya.sinha@tothenew.com>
2026-03-28 22:27:06 +0530

    added database module

---
+++ terraform/modules/database/main.tf
@@ -0,0 +1,15 @@
+resource "aws_db_subnet_group" "db_group" {
+  name       = "test-db-subnet-group"
+  subnet_ids = var.private_subnets
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier        = "app-db"
+  engine            = "postgres"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 20
+  db_subnet_group_name = aws_db_subnet_group.db_group.name
+  username          = var.db_user
+  password          = var.db_password
+  skip_final_snapshot = true
+}
