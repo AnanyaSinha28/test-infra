4d2c86be85fb060bda3e80d693eaf5de6acfeba8
Ananya Sinha <ananya.sinha@tothenew.com>
2026-03-28 22:40:34 +0530

    updated cluster

--- terraform/modules/cluster/main.tf
+++ terraform/modules/cluster/main.tf
@@ -1,8 +1,22 @@
 resource "aws_ecs_cluster" "main" {
-  name = "test-app-cluster"
+  name = var.cluster_name
+}
+
+resource "aws_ecs_capacity_provider" "main" {
+  name = "asg-capacity-provider"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = var.asg_arn
+    managed_termination_protection = "DISABLED"
+
+    managed_scaling {
+      status          = "ENABLED"
+      target_capacity = 80
+    }
+  }
 }
 
 resource "aws_ecs_cluster_capacity_providers" "main" {
-  cluster_name = aws_ecs_cluster.main.name
-  capacity_providers = ["EC2"]
+  cluster_name       = aws_ecs_cluster.main.name
+  capacity_providers = [aws_ecs_capacity_provider.main.name]
 }
